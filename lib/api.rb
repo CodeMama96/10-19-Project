7d7122fa78346f935882bf871e431fb4d2c4afa5
Alicia Weenum <weenum.alicia@gmail.com>
2020-11-09 11:15:34 -0500

    first commit

--- lib/api.rb
+++ lib/api.rb
@@ -4,13 +4,13 @@ class API
         puts "What drink would you like today?"
         input = gets.strip
         url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=martini" #http://makeup-api.herokuapp.com/api/v1/products.json
-        
+
         response = Net::HTTP.get(uri)
         hash = JSON.parse(response)
-        array_of_drinks = hash["drinks"]
-        array_of_drinks.each do |drink|
+        array_of_makeup = hash["products"]
+        array_of_makeup.each do |makeup|
                 # id, name, category, instructions
-                Drink.new(drink["idDrink"], drink["strDrink"], drink["strCategory"], drink["strInstructions"])
+                MakeUp.new(makeup["idDrink"], makeup["strDrink"], makeup["strCategory"], makeup["strInstructions"])
         end
     end
 end
--- lib/make_up.rb
+++ lib/make_up.rb
@@ -1,4 +1,4 @@
-class Drink
+class MakeUp
 
     attr_accessor :id, :name, :category, :instructions
 
