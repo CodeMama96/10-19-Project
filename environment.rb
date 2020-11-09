42209bec2854c6697bebfdb5dff15742c5326cf0
Alicia Weenum <weenum.alicia@gmail.com>
2020-11-09 15:15:53 -0500

    otheredits

--- environment.rb
+++ environment.rb
@@ -10,4 +10,4 @@ Bundler.require
 
 require_relative "./lib/api"
 require_relative "./lib/cli"
-require_relative "./lib/make_up"
+require_relative "./lib/makeup"
--- lib/makeup.rb
+++ lib/makeup.rb
@@ -2,15 +2,15 @@ class MakeUp
 
     attr_accessor :name, :brand, :price, :product
 
-    # @@all = []
-    # def initialize(name, brand, price, product)
-    #     @name = name
-    #     @brand = brand
-    #     @price = price
-    #     @product = product
+    @@all = []
+    def initialize(name, brand, price, product)
+        @name = name
+        @brand = brand
+        @price = price
+        @product = product
 
-    #     @@all << self
-    # end
+        @@all << self
+    end
 
     # def self.all
     #     @@all
